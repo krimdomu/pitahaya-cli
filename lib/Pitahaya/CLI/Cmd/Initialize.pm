f6369384f4b2bb4f2ed08ef1b38ab7d3f530b35b
krimdomu <krimdomu@googlemail.com>
2016-02-20 14:54:56 +0100

    config file

--- lib/Pitahaya/CLI/Cmd/Initialize.pm
+++ lib/Pitahaya/CLI/Cmd/Initialize.pm
@@ -10,51 +10,50 @@ use Digest::SHA256;
 use IO::All;
 
 use File::Basename 'dirname';
-
 use Data::Dumper;
 
 option 'url' => (
-    is       => 'ro',
-    format   => 's',
-    required => 1,
-    doc      => 'URL to Pitahaya server',
+  is       => 'ro',
+  format   => 's',
+  required => 1,
+  doc      => 'URL to Pitahaya server',
 );
 
 option 'user' => (
-    is       => 'ro',
-    format   => 's',
-    required => 1,
-    doc      => 'User to use for querying api',
+  is       => 'ro',
+  format   => 's',
+  required => 1,
+  doc      => 'User to use for querying api',
 );
 
 option 'password' => (
-    is       => 'ro',
-    format   => 's',
-    required => 1,
-    doc      => 'Password to use for querying api',
+  is       => 'ro',
+  format   => 's',
+  required => 1,
+  doc      => 'Password to use for querying api',
 );
 
 option 'site_name' => (
-    is       => 'ro',
-    format   => 's',
-    required => 1,
-    doc      => 'Which site to syncronize',
+  is       => 'ro',
+  format   => 's',
+  required => 1,
+  doc      => 'Which site to syncronize',
 );
 
 sub execute {
-    my ( $self, $args_ref, $chain_ref ) = @_;
+  my ( $self, $args_ref, $chain_ref ) = @_;
 
-    mkdir ".pitahaya";
-    open( my $fh, ">", ".pitahaya/config" ) or die($!);
-    print $fh JSON::XS::encode_json(
-        {
-            url       => $self->url,
-            user      => $self->user,
-            password  => $self->password,
-            site_name => $self->site_name,
-        }
-    );
-    close($fh);
+  mkdir ".pitahaya";
+  open( my $fh, ">", ".pitahaya/config" ) or die($!);
+  print $fh JSON::XS::encode_json(
+    {
+      url       => $self->url,
+      user      => $self->user,
+      password  => $self->password,
+      site_name => $self->site_name,
+    }
+  );
+  close($fh);
 }
 
 1;
--- lib/Pitahaya/CLI/Cmd/Pull.pm
+++ lib/Pitahaya/CLI/Cmd/Pull.pm
@@ -17,38 +17,38 @@ use Data::Dumper;
 has app => ( is => 'rwp', );
 
 sub execute {
-    my ( $self, $args_ref, $chain_ref ) = @_;
+  my ( $self, $args_ref, $chain_ref ) = @_;
 
-    my $app = $self->command_chain->[0];
-    $self->_set_app($app);
+  my $app = $self->command_chain->[0];
+  $self->_set_app($app);
 
-    my $cfg = $self->app->config;
+  my $cfg = $self->app->config;
 
-    my $site_o    = $app->get_site( $cfg->{site_name} );
-    my $root_page = $site_o->get_page( $site_o->root_page_id );
-    $self->app->update_page( $root_page, "." );
-    $self->_walk_children( $root_page, "." );
+  my $site_o    = $app->get_site( $cfg->{site_name} );
+  my $root_page = $site_o->get_page( $site_o->root_page_id );
+  $self->app->update_page( $root_page, "." );
+  $self->_walk_children( $root_page, "." );
 
-    $self->_generate_sha_db;
+  $self->_generate_sha_db;
 }
 
 sub _walk_children {
-    my ( $self, $page_o, $path ) = @_;
+  my ( $self, $page_o, $path ) = @_;
 
-    for my $child_o ( $page_o->children ) {
-        $self->app->update_page( $child_o, $path );
-        $self->_walk_children( $child_o, $path . "/" . $child_o->url );
-    }
+  for my $child_o ( $page_o->children ) {
+    $self->app->update_page( $child_o, $path );
+    $self->_walk_children( $child_o, $path . "/" . $child_o->url );
+  }
 }
 
 sub _generate_sha_db {
-    my ($self) = @_;
+  my ($self) = @_;
 
-    my $db = $self->command_chain->[0]->get_sha_sums;
+  my $db = $self->command_chain->[0]->get_sha_sums;
 
-    open( my $db_fh, ">", ".pitahaya/sha.db" ) or die($!);
-    print $db_fh JSON::XS::encode_json($db);
-    close($db_fh);
+  open( my $db_fh, ">", ".pitahaya/sha.db" ) or die($!);
+  print $db_fh JSON::XS::encode_json($db);
+  close($db_fh);
 }
 
 1;
